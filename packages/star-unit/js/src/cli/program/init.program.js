f0aee828f0c375a6a145a23255d7722d84b02130
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-03-29 14:35:49 +0100

    Add todo to init configuration

--- packages/star-unit/js/src/cli/program/init.program.js
+++ packages/star-unit/js/src/cli/program/init.program.js
@@ -1,10 +1,11 @@
 const { StepRunner } = require("../system/step-runner");
-const {AStep} = require("../steps/a.step");
-const {BStep} = require("../steps/b.step");
+const { AStep } = require("../steps/a.step");
+const { BStep } = require("../steps/b.step");
 
 class InitProgram {
     run() {
         const stepRunner = new StepRunner();
+        // todo: Add steps to init configuration
         stepRunner.run([AStep, BStep]);
     }
 }
