8885f4412bf8576e84818d609a67c15705d51343
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-05-04 10:45:08 +0200

    feat: Publish new version to the npm registry

--- packages/star-unit/js/src/domain/program.domain.js
+++ packages/star-unit/js/src/domain/program.domain.js
@@ -1,7 +1,7 @@
 const programDomain = {
   name: "Star Unit (for JavaScript)",
   description: "A lightweight tool for instant JavaScript unit testing.",
-  version: "1.2.2",
+  version: "1.2.3",
 };
 
 module.exports = { programDomain };
