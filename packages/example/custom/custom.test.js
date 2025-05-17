6445023b5a23730239c96b003570a23c1332d339
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-05-17 10:10:23 +0200

    chore: add empty line

--- packages/example/custom/custom.test.js
+++ packages/example/custom/custom.test.js
@@ -4,6 +4,7 @@ describe("custom group", () => {
   test("should be even").params([2, 4, 6, 8, 10]).create((param) => {
     expect(param).custom((param) => param % 2 === 0);
   });
+
   test("should be odd").params([1, 3, 5, 7, 9]).create((param) => {
     expect(param).not.custom((param) => param % 2 === 0);
   });
